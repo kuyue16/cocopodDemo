6edba5cd1cdbfacaec7b8b58d2680c154cb46b2a
风起故里 <kuyue16@163.com>
2016-04-07 11:08:28 +0800

    添加spec

---
+++ cocopod/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  cocopod
+//
+//  Created by 风起故里 on 16/4/7.
+//  Copyright © 2016年 data. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ cocopod/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  cocopod
+//
+//  Created by 风起故里 on 16/4/7.
+//  Copyright © 2016年 data. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
